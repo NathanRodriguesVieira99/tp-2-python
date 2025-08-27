cdfd2e6809de568198b74970d6a946ee0adcbe47
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-27 08:49:20 -0300

    feat: exercices 8 and 9 done

--- ex8.py
+++ ex8.py
@@ -1,1 +1,42 @@
-# def reorganizar_fila():
+# Cenário A
+pedidos = ['P123', 'P456', 'P789']
+pedido_a_substituir = 'P456'
+prioridade_urgente = True
+pedido_urgente = 'P999'
+
+
+if prioridade_urgente:
+    pedidos.insert(0, pedido_urgente)
+print('Fila final (Cenário A): ', pedidos)
+
+
+# Cenário B
+pedidos = ['P123', 'P456', 'P789']
+pedido_a_substituir = 'P456'
+prioridade_urgente = False
+pedido_urgente = 'P999'
+
+if prioridade_urgente:
+    pedidos.insert(0, pedido_urgente)
+else:
+    if pedido_a_substituir in pedidos:
+        index = pedidos.index(pedido_a_substituir)
+        pedidos[index] = pedido_urgente
+print('Fila final (Cenário B): ', pedidos)
+
+
+# Cenário C
+pedidos = ['P123', 'P888', 'P789']
+pedido_a_substituir = 'P456'
+prioridade_urgente = False
+pedido_urgente = 'P999'
+
+if prioridade_urgente:
+    pedidos.insert(0, pedido_urgente)
+else:
+    if pedido_a_substituir in pedidos:
+        index = pedidos.index(pedido_a_substituir)
+        pedidos[index] = pedido_urgente
+    else:
+        print('Pedido a substituir não está na lista. Nenhuma alteração feita.')
+print('Fila final (Cenário C):', pedidos)
--- ex9.py
+++ ex9.py
@@ -1,1 +1,14 @@
-# def unificar_estoque():
+produtos_a = ['banana', 'maçã']
+produtos_b = ['laranja', 'pera']
+produtos_c = ['laranja', 'banana', 'maçã', 'romã']
+
+
+def unificar_estoque(lista1, lista2, lista3):
+    estoque_total = lista1 + lista2 + lista3
+    print('Estoque total: ', estoque_total)
+
+    for produto in set(estoque_total):
+       print(f"{produto}: {estoque_total.count(produto)} unidade(s)")
+
+print(unificar_estoque(produtos_a, produtos_b, produtos_c)
+      )
